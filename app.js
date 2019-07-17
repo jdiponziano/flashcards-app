bd7069776335006b66c02dcfc3cd14f44be5aa58
Jessica DiPonziano <jess@webdevjd.com>
2019-07-17 16:11:21 -0400

    Set template language to pug

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.set('view engine', 'pug');
+
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(port, () => console.log(`Application is running on localhost:${port}!`));
