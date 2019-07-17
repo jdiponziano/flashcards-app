cac72cdf0b246a6b755f1771e79b5629d07eafa9
Jessica DiPonziano <jess@webdevjd.com>
2019-07-17 16:34:25 -0400

    Add first flashcard

--- app.js
+++ app.js
@@ -6,5 +6,8 @@ const port = 3000;
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => res.render('index'));
+app.get('/cards', (req, res) => {
+  res.render('card', { prompt: "Who is buried in Grant's tomb?", hint: "Think about whose tomb it is." });
+});
 
 app.listen(port, () => console.log(`Application is running on localhost:${port}!`));
