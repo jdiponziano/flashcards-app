014be8029a9bee94ecbc4e4242d0e1e533d0a198
Jessica DiPonziano <jess@webdevjd.com>
2019-07-17 17:15:00 -0400

    Add form to get username

--- app.js
+++ app.js
@@ -10,4 +10,6 @@ app.get('/cards', (req, res) => {
   res.render('card', { prompt: "Who is buried in Grant's tomb?", hint: "Think about whose tomb it is." });
 });
 
+app.get('/hello', (req, res) => res.render('hello'));
+
 app.listen(port, () => console.log(`Application is running on localhost:${port}!`));
