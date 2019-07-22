40b6c0769ab1fe8bc7c88e59c646a1c9277428f0
Jessica DiPonziano <jess@webdevjd.com>
2019-07-22 17:36:14 -0400

    Add username to welcome message

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 //Variables
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => res.render('index'));
+
 app.get('/cards', (req, res) => {
   res.render('card', { prompt: "Who is buried in Grant's tomb?", hint: "Think about whose tomb it is." });
 });
 
 app.get('/hello', (req, res) => res.render('hello'));
+
+app.post('/hello', (req, res) => {
+  res.render('hello', { name: req.body.username });
+});
 
 app.listen(port, () => console.log(`Application is running on localhost:${port}!`));
