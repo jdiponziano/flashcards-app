8f705845d4bf6e28cc71918b3f3808f4a848d592
Jessica DiPonziano <jess@webdevjd.com>
2019-07-27 16:28:17 -0400

    Add name to all pages

--- routes/cards.js
+++ routes/cards.js
@@ -17,9 +17,10 @@ router.get('/:id', (req, res) => {
     res.redirect(`/cards/${id}?side=question`);
   }
 
+  const name = req.cookies.username;
   const text = cards[id][side];
   const { hint } = cards[id];
-  const templateData = { id, text };
+  const templateData = { id, text, name };
 
   if (side === 'question') {
     templateData.hint = hint;
