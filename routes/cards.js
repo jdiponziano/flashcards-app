524c73b1875b1ff55091145e7d2334f8dd761e5c
Jessica DiPonziano <jess@webdevjd.com>
2019-07-27 15:59:39 -0400

    Randomize cards and redirect for /cards

--- routes/cards.js
+++ routes/cards.js
@@ -3,15 +3,26 @@ const router = express.Router();
 const { data } = require('../data/flashcardData.json');
 const { cards } = data;
 
+router.get('/', (req, res) => {
+  const numOfCards = cards.length;
+  const flashcardId = Math.floor(Math.random() * numOfCards);
+  res.redirect(`/cards/${flashcardId}?side=question`);
+});
+
 router.get('/:id', (req, res) => {
   const { side } = req.query;
   const { id } = req.params;
   const text = cards[id][side];
   const { hint } = cards[id];
-  const templateData = { text };
+  const templateData = { id, text };
 
   if (side === 'question') {
     templateData.hint = hint;
+    templateData.sideToShow = 'answer';
+    templateData.sideToShowDisplay = 'Answer';
+  } else if (side === 'answer') {
+    templateData.sideToShow = 'question';
+    templateData.sideToShowDisplay = 'Question';
   }
 
   res.render('card', templateData);
