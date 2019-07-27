21063ff3a14c268415538bbae8d045f428cb3930
Jessica DiPonziano <jess@webdevjd.com>
2019-07-27 14:44:31 -0400

    Add custom error handling

--- app.js
+++ app.js
@@ -42,4 +42,16 @@ app.post('/goodbye', (req, res) => {
   res.redirect('/hello');
 });
 
+app.use((req, res, next) => {
+  const err = new Error('Not found');
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  res.locals.error = err;
+  res.status(err.status);
+  res.render('error');
+});
+
 app.listen(port, () => console.log(`Application is running on localhost:${port}!`));
