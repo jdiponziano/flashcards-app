e63755a983306cc181820b1dab381f81806a89c7
Jessica DiPonziano <jess@webdevjd.com>
2019-07-27 13:54:42 -0400

    Add redirects based on existence of username

--- app.js
+++ app.js
@@ -10,19 +10,26 @@ app.use(cookieParser());
 
 app.set('view engine', 'pug');
 
-app.get('/', (req, res) => res.render('index'));
+app.get('/', (req, res) => {
+  const name = req.cookies.username;
+  if (name) {
+    res.render('index', { name });
+  } else {
+    res.redirect('/hello');
+  }
+});
 
 app.get('/cards', (req, res) => {
   res.render('card', { prompt: "Who is buried in Grant's tomb?", hint: "Think about whose tomb it is." });
 });
 
 app.get('/hello', (req, res) => {
-  res.render('hello', { name: req.cookies.username });
+  res.render('hello');
 });
 
 app.post('/hello', (req, res) => {
   res.cookie('username', req.body.username);
-  res.render('hello', { name: req.body.username });
+  res.redirect('/');
 });
 
 app.listen(port, () => console.log(`Application is running on localhost:${port}!`));
